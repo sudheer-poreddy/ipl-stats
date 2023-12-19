6e304ed2838278ce06f65b41686bdb4b36107508
Sudheer Reddy <153163355+sudheer-poreddy@users.noreply.github.com>
2023-12-20 00:12:32 +0530

    onclick

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,13 @@ button {
 """,
     unsafe_allow_html=True,
 )
-
+def dream11():
+    st.success('you have selected dream11')
 
 st.sidebar.button("Tourney Stats", use_container_width=True)
 st.sidebar.button("Match stats", use_container_width=True)
 st.sidebar.button("Player Stats", use_container_width=True)
-st.sidebar.button("Dream11", use_container_width=True)
+st.sidebar.button("Dream11", on_click= dream11, use_container_width=True)
+
+def dream11():
+    st.success('you have selected dream11')
